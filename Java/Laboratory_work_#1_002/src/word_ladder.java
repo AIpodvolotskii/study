3e9eea3b3b4f25536de87813d339ff6e9dc79cef
AIpodvolostkii <iuranovitcuria83@gmail.com>
2018-10-15 12:03:57 +0300

    Second commit

---
+++ Java/Laboratory_work_#1_002/src/word_ladder.java
@@ -0,0 +1,11 @@
+
+public class word_ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vasya");
+		System.out.println("	went for");
+		System.out.println("		a walk");
+	}
+
+}
