af0426e7554013b5de9cef6cf3f07800f6ef7472
AIpodvolostkii <iuranovitcuria83@gmail.com>
2018-10-15 13:22:29 +0300

    First commit

---
+++ Java/Laboratory_work_#1_003/src/drawing_of_letters.java
@@ -0,0 +1,15 @@
+
+public class drawing_of_letters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("   M   ");
+		System.out.println("  MMM  ");
+		System.out.println(" MMMMM ");
+		System.out.println("MMMMMMM");
+		System.out.println(" HH HH ");
+		System.out.println(" ZZZZZ ");
+		
+	}
+
+}
---
+++ Java/Laboratory_work_#1_004/src/boolean_calculations.java
@@ -0,0 +1,15 @@
+
+public class boolean_calculations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a, b, c;
+		
+		a = true;
+		b = !a;
+		c = a || b && !a;
+		
+		System.out.println(c);
+	}
+
+}
